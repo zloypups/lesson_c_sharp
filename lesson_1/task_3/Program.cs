cd52ae7e0619361a54c9bbb6159ecd034aad4f64
zloypups <prock88@mail.ru>
2022-12-01 19:52:06 +0300

    решение задачи task_3

--- lesson_1/task_3/Program.cs
+++ lesson_1/task_3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.Clear();
+Console.Write("Введите число для провеки на чет/нечет: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine($"{a}, это четное число");
+}
+else
+    Console.WriteLine($"{a}, это не четное число");
