2284f2defa4e738e0257672cd74cb9f68dbaf4f1
zloypups <prock88@mail.ru>
2022-12-03 11:25:15 +0300

    решение задачи task_1

--- lesson_2/task_1/Program.cs
+++ lesson_2/task_1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+
+int x1 = x / 10;
+int x2 = x1 % 10;
+
+Console.WriteLine(x2);
+// Console.WriteLine(x1 % 10);
