f43f85e71bbc1394a655258264e00da5e041624b
zloypups <prock88@mail.ru>
2022-12-03 14:39:22 +0300

    исправление  решения задачи task_2

--- lesson_2/task_2/Program.cs
+++ lesson_2/task_2/Program.cs
@@ -14,9 +14,14 @@ Console.Write("Введите число: ");
 int x = int.Parse(Console.ReadLine());
 
 int x1 = x % 10;
-if (x >= 100)
+int x2 = x1 % 10;
+if (x >= 100 && x <= 999)
 {
     Console.WriteLine(x1);
+}
+else if (x >= 1000)
+{
+    Console.WriteLine(x2);
 }
 else
     Console.WriteLine("Это меньше чем трехзначное число");
