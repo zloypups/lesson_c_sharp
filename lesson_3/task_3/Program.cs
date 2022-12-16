9716756861d3af5026091faa757a3f19fda6f1a8
zloypups <prock88@mail.ru>
2022-12-16 19:39:13 +0300

    Решение задичи  task_3

--- lesson_3/task_3/Program.cs
+++ lesson_3/task_3/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+    Console.Write($"{i * i * i} ");
