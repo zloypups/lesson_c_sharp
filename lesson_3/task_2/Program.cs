af072cd1d9691695ea1f549792c9973278ffbf45
zloypups <prock88@mail.ru>
2022-12-16 19:08:37 +0300

    Решение задичи  task_2

--- lesson_3/task_2/Program.cs
+++ lesson_3/task_2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+Console.Write("Введите координату X первой точки: ");
+Double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y первой точки: ");
+Double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z первой точки: ");
+Double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату X Второй точки: ");
+Double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y Второй точки: ");
+Double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z Второй точки: ");
+Double z2 = Convert.ToDouble(Console.ReadLine());
+
+double dist = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+Console.WriteLine(Math.Round(dist, 2));
