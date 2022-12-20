270e1623602eaf8174ca28208b948416e72979ae
zloypups <prock88@mail.ru>
2022-12-20 20:55:13 +0300

    решение задачи task_1

--- lesson_4/task_1/Program.cs
+++ lesson_4/task_1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Console.Clear();
+Console.Write("Введите число A: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int m = Convert.ToInt32(Console.ReadLine()), count = n;
+
+for (int i = 1; i < m; i++)
+{
+
+    count = count * n;
+}
+Console.WriteLine(count);
