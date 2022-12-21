5c189522506f295e98a3b51e8ee20376a308dfb6
zloypups <prock88@mail.ru>
2022-12-21 17:26:51 +0300

    решение задачи task_2

---
+++ lesson_5/task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+ Найдите сумму элементов, стоящих на нечётных позициях.
+
+ [3, 7, 23, 12] -> 19
+
+ [-4, -6, 89, 6] -> 0
+
+ */
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int result = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 11);
+
+    if (i % 2 == 1)
+    {
+        result = result + array[i];
+    }
+}
+Console.WriteLine($"Наш массив: [{string.Join(", ", array)}]");
+Console.WriteLine(result);
