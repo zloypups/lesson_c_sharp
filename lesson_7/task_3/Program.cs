5027b9d3974acc9f36c9eab920a550c3d4bdfbc9
zloypups <prock88@mail.ru>
2022-12-23 18:25:02 +0300

    решение задачи task_3

--- lesson_7/task_3/Program.cs
+++ lesson_7/task_3/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReliseMatrix(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+        }
+        Console.Write($"{sum / matrix.GetLength(0)};    ");
+    }
+
+}
+Console.Clear();
+int[,] matrix = new int[5, 7];
+
+InputMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine("------------------------------------------------------");
+Console.WriteLine();
+
+ReliseMatrix(matrix);
+Console.WriteLine();
+
+
