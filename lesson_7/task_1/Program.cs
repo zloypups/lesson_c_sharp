19afd8866bf85f20b37288df007f46341642a10d
zloypups <prock88@mail.ru>
2022-12-23 17:33:12 +0300

    решение задачи task_1

--- lesson_7/task_1/Program.cs
+++ lesson_7/task_1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (10 + 10) - 10, 2);
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы через пробел: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+double[,] matrix = new double[size[0], size[1]];
+InputMatrix(matrix);
